d25e5ba262a4ca8efff75fa292d86f3d12a6beea
Josiel Soares <76651121+JosielSSoares@users.noreply.github.com>
2025-03-20 21:09:45 -0400

    Create propriedadesRetangulo.java

---
+++ propriedadesRetangulo/propriedadesRetangulo.java
@@ -0,0 +1,27 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class propriedadesRetangulo {
+    public static void main(String[] args) throws Exception {
+
+        Locale.setDefault(Locale.US);
+        try (Scanner sc = new Scanner(System.in)) {
+            Locale.setDefault(Locale.US);
+
+            Retangulo retangulo = new Retangulo();
+
+            System.out.println("Insira a largura e a altura do retângulo:");
+            retangulo.largura = sc.nextDouble();
+            retangulo.altura = sc.nextDouble();
+
+            double area = retangulo.Area();
+            double perimetro = retangulo.Perimetro();
+            double diagonal = retangulo.Diagonal();
+
+
+            System.out.printf("\nÁrea: " + area + "\nPerímetro: " + perimetro + "\nDiagonal: " + diagonal);
+        }
+
+    }
+}
