efe392c92c4130adac8aadb3bf8d6eeb947eeab1
Josiel Soares <76651121+JosielSSoares@users.noreply.github.com>
2025-04-01 22:21:54 -0400

    Update Estoque.java

--- Estoque/Estoque.java
+++ Estoque/Estoque.java
@@ -1,34 +1,74 @@
-import java.util.Locale;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Estoque {
-    public static void main(String[] args) throws Exception {
-
+    public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            Locale.setDefault(Locale.US);
-            
-            System.out.println("Enter product data: ");
-            System.out.print("Name: ");
-            String name = sc.nextLine();
+            System.out.println("Entre com os dados do Produto");
 
-            System.out.print("Price:");
-            double price = sc.nextDouble();
+            String name;
+            while (true) {
+                System.out.print("Nome: ");
+                name = sc.nextLine();
+                try {
+                    break;
+                } catch (IllegalArgumentException e) {
+                    System.err.println("Erro no nome: " + e.getMessage());
+                }
+            }
 
+            double price;
+            while (true) {
+                System.out.print("Preço: ");
+                try {
+                    price = sc.nextDouble();
+                    break;
+                } catch (InputMismatchException e) {
+                    System.err.println("Erro no preço: Por favor, digite um número válido.");
+                    sc.next();
+                } catch (IllegalArgumentException e) {
+                    System.err.println("Erro no preço: " + e.getMessage());
+                }
+            }
+            sc.nextLine();
             Product product = new Product(name, price);
 
-            System.out.println("\nProduct data: " + product);
-            System.out.print("\nEnter the number of products to be added in stock: ");
-            int quantity = sc.nextInt();
-            product.addProducts(quantity);
-            
-            System.out.println("\nUpdated data: " + product);
+            System.out.println("\nDados do Produto: " + product);
 
-            System.out.print("\nEnter the number of products to be removed from stock: ");
-            quantity = sc.nextInt();
-            product.removeProducts(quantity);
-            System.out.println("\nUpdated data: " + product);
+            int quantityToAdd;
+            while (true) {
+                System.out.print("Qual a quantidade de produtos de deseja acrescentar? ");
+                try {
+                    quantityToAdd = sc.nextInt();
+                    product.addProducts(quantityToAdd);
+                    break;
+                } catch (InputMismatchException e) {
+                    System.err.println("Erro na quantidade: Por favor, digite um número inteiro.");
+                    sc.next();
+                } catch (IllegalArgumentException e) {
+                    System.err.println("Erro na quantidade: " + e.getMessage());
+                }
+            }
+            sc.nextLine();
 
-            sc.close();
+            System.out.println("\nDados atualizados do produto: " + product);
+
+            int quantityToRemove;
+            while (true) {
+                System.out.print("\nEntre com a quantidade que deseja REMOVER deste produto? ");
+                try {
+                    quantityToRemove = sc.nextInt();
+                    product.removeProducts(quantityToRemove);
+                    break;
+                } catch (InputMismatchException e) {
+                    System.err.println("Erro na quantidade: Por favor, digite um número inteiro.");
+                    sc.next();
+                } catch (IllegalArgumentException e) {
+                    System.err.println("Erro na quantidade: " + e.getMessage());
+                }
+            }
+
+            System.out.println("\nDados atualizados: " + product);
         }
     }
 }
