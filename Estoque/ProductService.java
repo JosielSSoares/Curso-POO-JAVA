75750912e2412319066bd050a00feed0706495a9
Josiel Soares <76651121+JosielSSoares@users.noreply.github.com>
2025-04-03 18:57:15 -0400

    Update ProductService.java
    Esta classe só deverá ser descomentada para utilização caso queira usar em um arquivo separado.

--- Estoque/ProductService.java
+++ Estoque/ProductService.java
@@ -1,14 +1,14 @@
-public class ProductService {
+// public class ProductService {
 
-    public Product createProduct(String name, double price) {
-        return new Product(name, price);
-    }
+//     public Product createProduct(String name, double price) {
+//         return new Product(name, price);
+//     }
 
-    public void addStock(Product product, int quantity) {
-        product.addProducts(quantity);
-    }
+//     public void addStock(Product product, int quantity) {
+//         product.addProducts(quantity);
+//     }
 
-    public void removeStock(Product product, int quantity) {
-        product.removeProducts(quantity);
-    }
-}
+//     public void removeStock(Product product, int quantity) {
+//         product.removeProducts(quantity);
+//     }
+// }
